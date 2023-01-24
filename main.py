0307194e079d5445966947f8f2822eed1a18f0ea
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-24 17:14:48 -0300

    feat: insert an user into table

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import mysql.connector
 
-connection = mysql.connector.connect(host='localhost', user='root', passwd='root', database='devpy')
+db = mysql.connector.connect(host='localhost', user='root', passwd='root', database='devpy')
 
-cursor = connection.cursor()
+cursor = db.cursor()
 
 # cursor.execute('CREATE DATABASE devpy')
 
-cursor.execute('CREATE TABLE users (name VARCHAR(255), age INTEGER(10))')
+# cursor.execute('CREATE TABLE users (name VARCHAR(255), age INTEGER(10))')
+
+sql_insert_command = 'INSERT INTO users (name, age) VALUES (%s, %s)'
+user = ('Thiago', 17)
+
+cursor.execute(sql_insert_command, user)
+
+db.commit()
