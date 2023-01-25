752b653de5514ab79091e02555d415108742b8ae
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-25 12:48:43 -0300

    feat: add test if CreateUserUsecase raises an error if parameters are emptys

--- src/usecases/create-user-usecase/TestCreateUserUsecase.py
+++ src/usecases/create-user-usecase/TestCreateUserUsecase.py
@@ -1,4 +1,19 @@
 import unittest
 
+class CreateUserUsecase():
+    def execute(self, name, email, password):
+        if name == '' or email == '' or password == '':
+            raise TypeError('Parameters are not provided correctyly! Fix it!')
+
 class TestCreateUserUsecase(unittest.TestCase):
-    pass
+    def test_it_have_raises_an_error_if_parameters_are_not_provided_correctly(self):
+            with self.assertRaises(TypeError) as error:
+                CreateUserUsecase().execute(name="", email="", password="")
+
+          
+            print(error)
+            self.assertEqual('Parameters are not provided correctyly! Fix it!', str(error.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
