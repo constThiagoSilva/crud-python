e3770a7e6e0a1bc5cd58a291a4695e9ad9c3e3b1
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-25 13:00:37 -0300

    feat: add test if database not provided in init

--- src/usecases/create-user-usecase/TestCreateUserUsecase.py
+++ src/usecases/create-user-usecase/TestCreateUserUsecase.py
@@ -1,18 +1,29 @@
 import unittest
 
 class CreateUserUsecase():
+    def __init__(self, database):
+        if database == '' or database == False:
+            raise TypeError('Database not provided! Fix it!')
+            
     def execute(self, name, email, password):
         if name == '' or email == '' or password == '':
             raise TypeError('Parameters are not provided correctyly! Fix it!')
 
 class TestCreateUserUsecase(unittest.TestCase):
-    def test_it_have_raises_an_error_if_parameters_are_not_provided_correctly(self):
-            with self.assertRaises(TypeError) as error:
-                CreateUserUsecase().execute(name="", email="", password="")
-
+    def test_it_have_an_database_parameter_in_init_def(self):
+        with self.assertRaises(TypeError) as error:
+            CreateUserUsecase(database='')
           
-            print(error)
-            self.assertEqual('Parameters are not provided correctyly! Fix it!', str(error.exception))
+        self.assertEqual('Database not provided! Fix it!', str(error.exception))
+        
+
+
+    def test_it_have_raises_an_error_if_parameters_are_not_provided_correctly(self):
+        with self.assertRaises(TypeError) as error:
+            CreateUserUsecase('any_database').execute(name="", email="", password="")
+          
+        self.assertEqual('Parameters are not provided correctyly! Fix it!', str(error.exception))
+    
 
 
 if __name__ == '__main__':
