58c31f8bb00651a04f8fd36cb4248374fe2646d9
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-30 20:39:19 -0300

    feat: add test to execute method of delete-user if parameter id not provided correctly

--- src/core/usecases/delete_by_id_user_usecase/TestDeleteByIdUserUsecase.py
+++ src/core/usecases/delete_by_id_user_usecase/TestDeleteByIdUserUsecase.py
@@ -1,4 +1,5 @@
 import unittest
+from src.core.usecases.mock.mock_database.MockDatabase import MockDatabase
 from src.core.interfaces.Database import Database
 
 class DeleteByIdUserUsecase:
@@ -7,8 +8,19 @@ class DeleteByIdUserUsecase:
             raise ValueError('Database Interface Not Provided')
         
         self.database = database
+    
+    def execute(self, id: int):
+        if not isinstance(id, int) or id <= 0:
+            raise ValueError('Parameter Id not Provided Correctly')
 
 class TestDeleteByIdUserUsecase(unittest.TestCase):
+    def test_if_parameter_id_has_provided_correctly_to_execute_method(self):
+        with self.assertRaises(ValueError) as error:
+            sut = DeleteByIdUserUsecase(MockDatabase)
+            sut.execute(id=0)
+        
+        self.assertEqual('Parameter Id not Provided Correctly', str(error.exception))
+
     def test_if_database_interface_has_passed_into_init_method(self):
         with self.assertRaises(ValueError) as error:
             DeleteByIdUserUsecase(database='')
