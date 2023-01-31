e6484072fcd598ab35b06e6537dfc9984f92151e
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-01-31 10:36:48 -0300

    feat: add sut

--- src/infra/database/TestDatabase.py
+++ src/infra/database/TestDatabase.py
@@ -3,6 +3,8 @@ from src.infra.database.Database import Database
 from src.infra.database.connection import connection
 
 class TestDatabase(unittest.TestCase):
+    sut = Database(connection, 'db_users_test')
+
     def test_if_hass_passed_connection_database_and_name_of_database_to_init_method(self):
         with self.assertRaises(TypeError) as error:
             Database(connection='', database_name='any_database_name')
