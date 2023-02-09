63f88fa8a591ce44b5f588ddbfa4c725cf66bf80
constThiagoSilva <thiago.p.dasilva2005@gmail.com>
2023-02-09 20:36:21 -0300

    feat: read al users into main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,38 @@
 from src.infra.database.connection import connection
 from src.infra.database.Database import Database
+from src.core.usecases.read_all_users_usecase.ReadAllUsersUsecase import ReadAllUserUsecase
 
 print("""Olá, esse é seu Gerenciador de Usuários em CMD \n""" + '-' * 60)
 database_name = input('Inicie seu banco com um nome: \n')
 table_name = input('Agora, insira o nome da tabela: \n')
 
 database = Database(connection, database_name, table_name, 'name VARCHAR(64)', 'email VARCHAR(64)', 'password VARCHAR(64)')
+
+database.create(('Thiago', 'thi@gmail.com', '123'))
+database.create(('Jorge', 'jor@gmail.com', '123'))
+
+while True:
+    print("""\nEscolha uma opção \n
+[1] - Adicionar um usuário      [3] - Ler usuários
+[2] - Atualizar um usuário      [4] - Deletar um usuário""")
+
+    users_choice = int(input())
+
+    match users_choice:
+        case 1:
+            print('Add')
+        case 2:
+            print('Up')
+        case 3:
+            users = ReadAllUserUsecase(database).execute()
+            print(users)
+            
+        case 4:
+            print('Del')
+        case _:
+            print('INisra um dos numeros')
+
+    break
 
 database.drop_database()
 database.close_cursor()
